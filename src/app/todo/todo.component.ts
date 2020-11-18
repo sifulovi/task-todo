ffb64ecfaf35d9a5d68600fc2255533637c60d3e
nestedbrains <isiful.ovi@gmail.com>
2020-11-18 13:52:00 +0600

    cleanup

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -42,10 +42,10 @@ export class TodoComponent implements OnInit {
     SaveOrUpdateDept(Todo: ITodo) {
         if (this.Todo.Id === -1) {
             this.service.add(Todo);
-            UIkit.notification({message: 'Save Successfully', pos: 'top-right', status: 'success'});
+            //UIkit.notification({message: 'Save Successfully', pos: 'top-right', status: 'success'});
         } else {
             this.service.update(Todo);
-            UIkit.notification({message: 'Update Successfully', pos: 'top-right', status: 'success'});
+          //  UIkit.notification({message: 'Update Successfully', pos: 'top-right', status: 'success'});
         }
         this.getTodoList();
     }
@@ -59,7 +59,7 @@ export class TodoComponent implements OnInit {
         this.service.delete(id);
         this.getTodoList();
         this.Todo = {Id: 0, Task: '', Done: false};
-        UIkit.notification({message: 'Save Successfully', pos: 'top-right', status: 'success'});
+     //   UIkit.notification({message: 'Save Successfully', pos: 'top-right', status: 'success'});
     }
 
     doneTask(Todo: Todo) {
